91d9413cc9f1b560fe26119a40f13fe32dbcba36
tweep <jan.vogel@gmail.com>
2017-06-07 08:10:35 -0700

    - changes

--- ledstrip-paralell/ledstrip-paralell.ino
+++ ledstrip-paralell/ledstrip-paralell.ino
@@ -6,25 +6,31 @@
 // Working version 0.1
 
 // Parameter 1 = number of pixels in strip
-// Parameter 2 = Arduino pin number (most are valid)
+// Parameter 2 = Arduino pin number (most are valid)   // done: 3, 5, 
 // Parameter 3 = pixel type flags, add together as needed:
 //   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
 //   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
 //   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
 //   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
-Adafruit_NeoPixel strip  = Adafruit_NeoPixel(60, PIN5, NEO_GRB + NEO_KHZ800);
-Adafruit_NeoPixel strip2 = Adafruit_NeoPixel(60, PIN4, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip  = Adafruit_NeoPixel(92, 3, NEO_GRB + NEO_KHZ800);
+
+// pin nrLED
+//  2   12
+//  3   92 
+//  
+
+//Adafruit_NeoPixel strip2 = Adafruit_NeoPixel(60, PIN4, NEO_GRB + NEO_KHZ800);
 
 void setup() {
  
   strip.begin();
   strip.show(); // Initialize all pixels to 'off'
 
-  strip2.begin();
-  strip2.show(); // Initialize all pixels to 'off'
+ // strip2.begin();
+ // strip2.show(); // Initialize all pixels to 'off'
 
   colorWipe(strip.Color(255, 0, 0), 3); // Red
-  colorWipe2(strip2.Color(0, 0, 255), 3); // blue
+ // colorWipe2(strip2.Color(0, 0, 255), 3); // blue
 }
 
 void loop() {
@@ -40,10 +46,10 @@ void loop() {
  //  theaterChase(strip.Color(0, 0, 127), 50); // Blue
 
  // rainbow(20);
-  rainbowCycle(10);
-  rainbowCycle2(10);
+   rainbowCycle(15);
+ // rainbowCycle2(10);
 
- // theaterChaseRainbow(50);
+ //theaterChaseRainbow(50);
 }
 
 // Slightly different, this makes the rainbow equally distributed throughout
@@ -60,19 +66,6 @@ void rainbowCycle(uint8_t wait) {
 }
 
 
-// Slightly different, this makes the rainbow equally distributed throughout
-void rainbowCycle2(uint8_t wait) {
-  uint16_t i, j;
-
-  for(j=0; j<256*5; j++) { // 5 cycles of all colors on wheel
-    for(i=0; i< strip2.numPixels(); i++) {
-      strip2.setPixelColor(i, Wheel(((i * 256 / strip2.numPixels()) + j) & 255));
-    }
-    strip2.show();
-    delay(wait);
-  }
-}
-
 // Fill the dots one after the other with a color
 void colorWipe(uint32_t c, uint8_t wait) {
   for(uint16_t i=0; i<strip.numPixels(); i++) {
@@ -82,14 +75,7 @@ void colorWipe(uint32_t c, uint8_t wait) {
   }
 }
 
-// Fill the dots one after the other with a color
-void colorWipe2(uint32_t c, uint8_t wait) {
-  for(uint16_t i=0; i<strip2.numPixels(); i++) {
-    strip2.setPixelColor(i, c);
-    strip2.show();
-    delay(wait);
-  }
-}
+
 
 void rainbow(uint8_t wait) {
   uint16_t i, j;
