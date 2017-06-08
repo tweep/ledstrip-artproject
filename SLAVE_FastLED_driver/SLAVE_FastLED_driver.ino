4a2243216b907dd33f26016d4913a81727104df8
tweep <jan.vogel@gmail.com>
2017-06-07 23:47:37 -0700

    - new files

--- SLAVE_FastLED_driver/SLAVE_FastLED_driver.ino
+++ SLAVE_FastLED_driver/SLAVE_FastLED_driver.ino
@@ -1,5 +1,9 @@
 
-// ARDUINO LED SLAVE - drives the LEDS and knows all about transitions
+/* ARDUINO LED SLAVE 
+   - drives the LEDS strands
+   - knows all about the strands ( nr LEDs and PINs)
+   - implementes their actions (transitions)
+*/   
 
 #include <Wire.h>
 #include <EasyTransferI2C.h>
@@ -7,11 +11,17 @@
 
 FASTLED_USING_NAMESPACE
 #define COLOR_ORDER GRB
-#define NUM_STRIPS  4
+#define NUM_STRIPS  36
 #define ARRAY_SIZE(A) (sizeof(A) / sizeof((A)[0]))
 #define I2C_SLAVE_ADDRESS 9
 CRGB leds[NUM_STRIPS][60];
-byte nrLedsPerStrand[NUM_STRIPS]    = {60, 60, 60, 60};
+
+//                           STRAND    S1  S2  S3  S4  S5  S6  S7  S8  S9 S10 S11  S12 S13 S14 S15 S16 S17 S18 S19 S20  S21 S22 S23 S24 S25 S26 S27 S28 S29 S30 S31 S32 S33 S34 S35 S36
+byte nrLedsPerStrand[NUM_STRIPS]    = {13, 21, 46, 31, 12, 10, 11, 14, 19, 17, 12,  5, 90, 75,  60, 28, 15, 7,  12, 27, 14, 36,  15, 19, 14, 12, 12, 14, 41, 14, 8,  6, 8,   9, 12, 15 };
+//                              PIN    47  32  49   5  34  31  2    3   8  7   24   23  6  12   4   35  51  22  36  25  11  45   43  44  27  53  50  48  10  13  52  46 9    26 28, 29
+
+
+
 
 EasyTransferI2C ET;
 
@@ -20,7 +30,7 @@ EasyTransferI2C ET;
 // ---- global variables to store data for the PATTERNS
 boolean gSwitch               = true; // global variable to remember current ON/OFF status
 byte    gstrand               = 0;    // global variable to point to current strand.
-byte    gHue[NUM_STRIPS]      = {0, 0, 0, 0};  // rotating "base color" used by many of the patterns, so each strip can have different gHue
+byte    gHue[NUM_STRIPS]      = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0, 0, 0, 0, 0,0, 0, 0, 0, 0,0, 0, 0, 0, 0,0, 0, 0, 0, 0,0, 0, 0, 0, 0,0 };  // rotating "base color" used by many of the patterns, so each strip can have different gHue
 
 // TODO Master: have longer sleep timer in setup()
 // sleep timer - to turn LED off after 60 min.
@@ -48,12 +58,59 @@ CLEDController *controllers[NUM_STRIPS];
 void setup() {
   Serial.begin(115200);
 
-  // -------- FASTLED strand setup --------
+  // -------- FASTLED strand setup with PINs --------S
+//                                     S1  S2  S3  S4  S5  S6  S7  S8  S9 S10 S11  S12 S13 S14 S15 S16 S17 S18 S19 S20  S21 S22 S23 S24 S25 S26 S27 S28 S29 S30 S31 S32 S33 S34 S35 S36
+byte nrLedsPerStrand[NUM_STRIPS]    = {13, 21, 46, 31, 12, 10, 11, 14, 19, 17, 12,  5, 90, 75,  60, 28, 15, 7,  12, 27, 14, 36,  15, 19, 14, 12, 12, 14, 41, 14, 8,  6, 8,   9, 12, 15 };
+//                              PIN    47  32  49   5  34  31  2    3   8  7   24   23  6  12   4   35  51  22  36  25  11  45   43  44  27  53  50  48  10  13  52  46 9    26 28, 29
+  //                                       PIN
+  controllers[0] = &FastLED.addLeds<WS2811, 47, GRB>(leds[0], nrLedsPerStrand[0]).setCorrection(TypicalLEDStrip);
+  controllers[1] = &FastLED.addLeds<WS2811, 32, GRB>(leds[1], nrLedsPerStrand[1]).setCorrection(TypicalLEDStrip);
+  controllers[2] = &FastLED.addLeds<WS2811, 49, GRB>(leds[2], nrLedsPerStrand[2]).setCorrection(TypicalLEDStrip);
+  controllers[3] = &FastLED.addLeds<WS2811,  5, GRB>(leds[3], nrLedsPerStrand[3]).setCorrection(TypicalLEDStrip);
+  controllers[4] = &FastLED.addLeds<WS2811, 34, GRB>(leds[4], nrLedsPerStrand[4]).setCorrection(TypicalLEDStrip);
+  
+  controllers[5] = &FastLED.addLeds<WS2811, 31, GRB>(leds[5], nrLedsPerStrand[5]).setCorrection(TypicalLEDStrip);
+  controllers[6] = &FastLED.addLeds<WS2811,  2, GRB>(leds[6], nrLedsPerStrand[6]).setCorrection(TypicalLEDStrip);
+  controllers[7] = &FastLED.addLeds<WS2811,  3, GRB>(leds[7], nrLedsPerStrand[7]).setCorrection(TypicalLEDStrip);
+  controllers[8] = &FastLED.addLeds<WS2811,  8, GRB>(leds[8], nrLedsPerStrand[8]).setCorrection(TypicalLEDStrip);
+  controllers[9] = &FastLED.addLeds<WS2811,  7, GRB>(leds[9], nrLedsPerStrand[9]).setCorrection(TypicalLEDStrip);
 
-  controllers[0] = &FastLED.addLeds<WS2811, 4, GRB>(leds[0], nrLedsPerStrand[0]).setCorrection(TypicalLEDStrip);
-  controllers[1] = &FastLED.addLeds<WS2811, 5, GRB>(leds[1], nrLedsPerStrand[1]).setCorrection(TypicalLEDStrip);
-  controllers[2] = &FastLED.addLeds<WS2811, 6, GRB>(leds[2], nrLedsPerStrand[2]).setCorrection(TypicalLEDStrip);
-  controllers[3] = &FastLED.addLeds<WS2811, 7, GRB>(leds[3], nrLedsPerStrand[3]).setCorrection(TypicalLEDStrip);
+  controllers[10] = &FastLED.addLeds<WS2811, 24, GRB>(leds[10], nrLedsPerStrand[10]).setCorrection(TypicalLEDStrip);
+  controllers[11] = &FastLED.addLeds<WS2811, 23, GRB>(leds[11], nrLedsPerStrand[11]).setCorrection(TypicalLEDStrip);
+  controllers[12] = &FastLED.addLeds<WS2811,  6, GRB>(leds[12], nrLedsPerStrand[12]).setCorrection(TypicalLEDStrip);
+  controllers[13] = &FastLED.addLeds<WS2811, 12, GRB>(leds[13], nrLedsPerStrand[13]).setCorrection(TypicalLEDStrip);
+  controllers[14] = &FastLED.addLeds<WS2811,  4, GRB>(leds[14], nrLedsPerStrand[14]).setCorrection(TypicalLEDStrip);
+
+  controllers[15] = &FastLED.addLeds<WS2811, 35, GRB>(leds[15], nrLedsPerStrand[15]).setCorrection(TypicalLEDStrip);
+  controllers[16] = &FastLED.addLeds<WS2811, 51, GRB>(leds[16], nrLedsPerStrand[16]).setCorrection(TypicalLEDStrip);
+  controllers[17] = &FastLED.addLeds<WS2811, 22, GRB>(leds[17], nrLedsPerStrand[17]).setCorrection(TypicalLEDStrip);
+  controllers[18] = &FastLED.addLeds<WS2811, 36, GRB>(leds[18], nrLedsPerStrand[18]).setCorrection(TypicalLEDStrip);
+  controllers[19] = &FastLED.addLeds<WS2811, 25, GRB>(leds[19], nrLedsPerStrand[19]).setCorrection(TypicalLEDStrip);
+
+
+  controllers[20] = &FastLED.addLeds<WS2811, 11, GRB>(leds[20], nrLedsPerStrand[20]).setCorrection(TypicalLEDStrip);
+  controllers[21] = &FastLED.addLeds<WS2811, 45, GRB>(leds[21], nrLedsPerStrand[21]).setCorrection(TypicalLEDStrip);
+  controllers[22] = &FastLED.addLeds<WS2811, 43, GRB>(leds[22], nrLedsPerStrand[22]).setCorrection(TypicalLEDStrip);
+  controllers[23] = &FastLED.addLeds<WS2811, 44, GRB>(leds[23], nrLedsPerStrand[23]).setCorrection(TypicalLEDStrip);
+  controllers[24] = &FastLED.addLeds<WS2811, 27, GRB>(leds[24], nrLedsPerStrand[24]).setCorrection(TypicalLEDStrip);
+
+  
+  controllers[25] = &FastLED.addLeds<WS2811, 53, GRB>(leds[25], nrLedsPerStrand[25]).setCorrection(TypicalLEDStrip);
+  controllers[26] = &FastLED.addLeds<WS2811, 50, GRB>(leds[26], nrLedsPerStrand[26]).setCorrection(TypicalLEDStrip);
+  controllers[27] = &FastLED.addLeds<WS2811, 48, GRB>(leds[27], nrLedsPerStrand[27]).setCorrection(TypicalLEDStrip);
+  controllers[28] = &FastLED.addLeds<WS2811, 10, GRB>(leds[28], nrLedsPerStrand[28]).setCorrection(TypicalLEDStrip);
+  controllers[29] = &FastLED.addLeds<WS2811, 13, GRB>(leds[29], nrLedsPerStrand[29]).setCorrection(TypicalLEDStrip);
+
+  
+  controllers[30] = &FastLED.addLeds<WS2811, 52, GRB>(leds[30], nrLedsPerStrand[30]).setCorrection(TypicalLEDStrip);
+  controllers[31] = &FastLED.addLeds<WS2811, 46, GRB>(leds[31], nrLedsPerStrand[31]).setCorrection(TypicalLEDStrip);
+  controllers[32] = &FastLED.addLeds<WS2811,  9, GRB>(leds[32], nrLedsPerStrand[32]).setCorrection(TypicalLEDStrip);
+  controllers[33] = &FastLED.addLeds<WS2811, 26, GRB>(leds[33], nrLedsPerStrand[33]).setCorrection(TypicalLEDStrip);
+  controllers[34] = &FastLED.addLeds<WS2811, 28, GRB>(leds[34], nrLedsPerStrand[34]).setCorrection(TypicalLEDStrip);
+
+  controllers[35] = &FastLED.addLeds<WS2811, 29, GRB>(leds[35], nrLedsPerStrand[35]).setCorrection(TypicalLEDStrip);
+
+
 
   for ( int j = 0; j < NUM_STRIPS; j++ ) {
     FastLED[j].showLeds(128); // set initial brightness of strip
@@ -88,7 +145,7 @@ void loop() {
      controllers[strip]->showLeds(ledConfig[strip].bright);
     
      Serial.println("Retrieving data...");
-     Serial.print("Strip: "); Serial.print(mydata.ledStripToConfigure);
+     Serial.print(" Strip: "); Serial.print(mydata.ledStripToConfigure);
      Serial.print(" pattern: "); Serial.print(mydata.pattern);
      Serial.print(" speed: "); Serial.print(mydata.spd);
      Serial.print(" brigh: "); Serial.print(mydata.bright);
--- ledstrip-paralell/ledstrip-paralell.ino
+++ ledstrip-paralell/ledstrip-paralell.ino
@@ -10,9 +10,9 @@
 // Parameter 3 = pixel type flags, add together as needed:
 //   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
 //   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
-//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
+//   NE3_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
 //   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
-Adafruit_NeoPixel strip  = Adafruit_NeoPixel(92, 3, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip  = Adafruit_NeoPixel(92, 53, NEO_GRB + NEO_KHZ800);
 
 // pin nrLED
 //  2   12
